fae8bcfb50dcea1986fe5d29936e748e64648269
AnuragChandra221 <pothaluranurag2002@gmail.com>
2023-03-11 00:31:31 +0530

    Project links updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -23,7 +23,7 @@ function Portfolio() {
         <p className='mt-5 pt-3 mb-4' style={{textAlign: 'start', color: '#fff', fontSize: 'xx-large', fontWeight: '900'}} >Portfolio</p>
         <Projects title="Timeline Calendar" description="This was a front end project REACT JS which shows the functionalities of a
             calendar with timeline. Later I added Django and made it a full stack project. The backend is hosted on Railway with a postgress database and the frontend is hosted on surge. "
-            live="https://timelinecalendar.surge.sh"
+            live="https://timelinecalendar.netlify.app"
             github="https://github.com/Anuragchandra221/Calendar-React-Js"
             img={require('../assets/calendar home.png')}
         />
@@ -32,7 +32,7 @@ function Portfolio() {
           <Projects title="Movie Mania" description="Movie Mania is an TMDB clone website which use TMDB api to fetch
               data. This site was built using REACT JS
               "
-              live="https://moviemania.surge.sh"
+              live="https://moviemac.netlify.app"
               github="https://github.com/Anuragchandra221/MovieMania"
               img={require('../assets/mm home.png')}
           />
