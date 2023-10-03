51aa96d0430d50f5e31c3698a35a518d07ba820d
Anuragchandra221 <pothaluranurag2002@gmail.com>
2023-10-04 00:03:56 +0530

    Updated resume and roles

--- src/Components/Roles.js
+++ src/Components/Roles.js
@@ -8,10 +8,10 @@ function Roles() {
         <p id="roles" className='mt-5 pt-3 mb-5' style={{textAlign: 'center', color: '#fff', fontSize: 'xx-large', fontWeight: '900'}} >Roles and Reslponsibilities</p>
         <div className='d-flex row'>
           <div className='col-md-6 justify-content-center justify-content-md-end'>
-            <RoleCard img={require('../assets/gdsc.png')} position="Web Lead" org="GDSC VAST" period="2023 Sep-Present"/>
+            <RoleCard img={require('../assets/gdsc.png')} position="Web Lead" org="GDSC VAST" period="2023-2024"/>
           </div>
           <div className='mt-3 mt-md-0 col-md-6 justify-content-center justify-content-md-end'>
-            <RoleCard img={require('../assets/mulearn.jpeg')} position="Web IG Lead" org="Gtech Mulearn VAST" period="2023 March-Present"/>
+            <RoleCard img={require('../assets/mulearn.jpeg')} position="Web IG Lead" org="Gtech Mulearn VAST" period="2023-2024"/>
           </div>
         </div>
         <div className='mt-3 mt-md-0 col-12 col-md-6 mx-auto'>
